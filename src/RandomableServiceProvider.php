45a23d79ceda4a2970d903b3bbb5924b5d7181c8
Dotun Jolaoso <dotunjolaosho@yahoo.com>
2019-12-11 02:23:08 +0000

    Apply fixes from StyleCI

--- src/RandomableServiceProvider.php
+++ src/RandomableServiceProvider.php
@@ -15,7 +15,7 @@ class RandomableServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/randomable.php', 'randomable');
+        $this->mergeConfigFrom(__DIR__.'/../config/randomable.php', 'randomable');
 
         $this->app->bind('randomable', function () {
             return new Randomable();
@@ -25,15 +25,15 @@ class RandomableServiceProvider extends ServiceProvider
     protected function publishResources()
     {
         $this->publishes([
-            __DIR__ . '/../config/randomable.php' => config_path('randomable.php'),
+            __DIR__.'/../config/randomable.php' => config_path('randomable.php'),
         ], 'randomable-config');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_randomables_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_randomables_table.php'),
+            __DIR__.'/../database/migrations/create_randomables_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_randomables_table.php'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/../database/seeds/RandomableTableSeeder.php' => database_path('seeds/RandomableTableSeeder.php'),
+            __DIR__.'/../database/seeds/RandomableTableSeeder.php' => database_path('seeds/RandomableTableSeeder.php'),
         ], 'seeds');
     }
 }
