06e552786e823481b47ef489cea689879c8e1ecc
Dotunj <dotunjolaosho@yahoo.com>
2019-12-15 13:43:58 +0100

    added tags

--- src/RandomableServiceProvider.php
+++ src/RandomableServiceProvider.php
@@ -30,10 +30,10 @@ class RandomableServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../database/migrations/create_randomables_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_randomables_table.php'),
-        ], 'migrations');
+        ], 'randomable-migrations');
 
         $this->publishes([
             __DIR__.'/../database/seeds/RandomableTableSeeder.php' => database_path('seeds/RandomableTableSeeder.php'),
-        ], 'seeds');
+        ], 'randomable-seeds');
     }
 }
